abadeca395a76dca83208d45696100f989841a1e
ptakondrej <florixakcz@seznam.cz>
2026-01-22 10:10:48 +0100

    Add InputEditor components to home route
    Updated the main page layout to include two InputEditor components side by side (or stacked on small screens) within a styled container.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,15 @@
+import InputEditor from '@/components/InputEditor'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <main></main>
+  return (
+    <main className="min-h-screen container mx-auto mt-8">
+      <div className="flex md:flex-row flex-col gap-4 w-full">
+        <InputEditor />
+        <InputEditor />
+      </div>
+    </main>
+  )
 }
