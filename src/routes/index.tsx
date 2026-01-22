40c3c5ebdff989d6c639cee0ac8731a22a027dbd
ptakondrej <florixakcz@seznam.cz>
2026-01-22 11:40:13 +0100

    Integrate OutputPreview and state into main route
    Added OutputPreview component and managed input state in the main route. InputEditor now receives input value, setter, and type props, enabling dynamic updates and preview functionality.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
 import InputEditor from '@/components/InputEditor'
+import OutputPreview from '@/components/OutputPreview'
+import { InputType } from '@/constants'
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
+  const [type, setType] = useState<InputType>('Auto-detect')
+  const [value, setValue] = useState<string>('')
+
   return (
     <main className="min-h-screen container mx-auto mt-8">
       <div className="flex md:flex-row flex-col gap-4 w-full">
-        <InputEditor />
-        <InputEditor />
+        <InputEditor
+          input={value}
+          setInput={setValue}
+          selectedType={type}
+          setSelectedType={setType}
+        />
+        <OutputPreview type={type} formattedOutput={value} />
       </div>
     </main>
   )
