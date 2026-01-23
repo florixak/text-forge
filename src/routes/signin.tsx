560fe27e4d055dc79aec5b48f8203ef97c722033
ptakondrej <florixakcz@seznam.cz>
2026-01-23 18:30:19 +0100

    Add login form and integrate with signin route
    Introduces a new LoginForm component with email/password and Google login options, including validation and toast notifications. Updates the /signin route to use the LoginForm and applies guest middleware for route protection.

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -1,9 +1,18 @@
+import { LoginForm } from '@/components/login-form'
+import { guestMiddleware } from '@/lib/middleware'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/signin')({
   component: RouteComponent,
+  server: {
+    middleware: [guestMiddleware],
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/signin"!</div>
+  return (
+    <section className="min-h-screen flex items-center justify-center max-w-md mx-auto p-4">
+      <LoginForm />
+    </section>
+  )
 }
